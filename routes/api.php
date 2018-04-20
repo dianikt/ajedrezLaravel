f7bb9fe6ae32ca62803489a2efcfd0cf82ac4398
super <dianikt7@gmail.com>
2018-04-20 20:22:30 +0200

    ya se mueven

--- routes/api.php
+++ routes/api.php
@@ -15,14 +15,17 @@ Route::post('/login', function(Request $request){
     if (Auth::attempt($credentials)) {  
     	$rand_part = str_shuffle('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789'.uniqid());
 		$ok = User::where('email', $credentials['email'])->update(['token' => $rand_part]);
+        $id = Auth::user()->id;    
 
-		return response()->json(['email' => $credentials['email'],'token' => $rand_part,'mensaje'=>1]);
+		return response()->json(['email' => $credentials['email'], 'idJugador1'=> $id,
+            'token' => $rand_part,'mensaje'=>1]);
 	}else {
 		return response()->json(['email' => $credentials['email'],'mensaje'=> 'Usuario No identificado!']);
 	}
 });
 
 Route::POST('/logout', function(Request $request){	
+
 
     $credentials = $request->only('email');
     
@@ -71,38 +74,45 @@ Route::POST('/registro', function(Request $request) {
 */     
 Route::POST('/initPartida', function(Request $request) {
 
-	$datosPartida = $request->only('idJugador1', 'idJugador2', 'init');
-	$fichas = [];
-	try {
-        $partida = new partidas;
-            $partida['jugador1'] = $datosPartida['idJugador1']; 
-            $partida['jugador2'] = $datosPartida['idJugador2'];             
-            $partida['estados'] = 1;
-        $partida->save();
+	$datosPartida = $request->only('idJugador1', 'idJugador2','token');
 
-        //jugador que ha iniciado sesion
-        $jugadorIni = $datosPartida['idJugador1']; 
-        //jugador elegigo de la lista
-        $jugadorEle = $datosPartida['idJugador2'];
+    $token = User::where('id', $datosPartida['idJugador1'])->get();
 
-        $fichas = posicionInicialFichas($partida['id'], $jugadorIni, $jugadorEle);
+    if( $token[0]['token'] == $datosPartida['token']){
+        $fichas = [];
+        try {
+            $partida = new partidas;
+                $partida['jugador1'] = $datosPartida['idJugador1']; 
+                $partida['jugador2'] = $datosPartida['idJugador2'];             
+                $partida['estados'] = 1;
+            $partida->save();
 
-        $estado = "Partida Init";
-        $mensaje = "A Jugar !! ";
-        $fichas;
+            //jugador que ha iniciado sesion
+            $jugadorIni = $datosPartida['idJugador1']; 
+            //jugador elegigo de la lista
+            $jugadorEle = $datosPartida['idJugador2'];
 
-    } catch (Exception $e) {
-        $estado = "Partida Exit";
-        $mensaje = $e->getMessage();
+            $fichas = posicionInicialFichas($partida['id'], $jugadorIni, $jugadorEle);       
+            
+            $ficha1 = fichas::where('idPartida', $partida['id'])->where('jugador',$datosPartida['idJugador1'])->get();
+
+            $ficha2 = fichas::where('idPartida', $partida['id'])->where('jugador',$datosPartida['idJugador2'])->get();
+
+        } catch (Exception $e) {
+            $estado = "Partida Exit";
+            $mensaje = $e->getMessage();
+        }
+
+        return response()->json(
+            [               
+              'ficha1' => $ficha1,
+              'ficha2' => $ficha2,
+              'estado' => $partida['estados']
+
+        ]);
+    }else {
+        return response()->json([ 'token'=>$token, 'mensaje' => 'El token no es coincidente']);
     }
-
-    return response()->json(
-        [               
-          'estado' => $estado, 
-          'mensaje' => $mensaje,
-          'partida' => $partida,
-          'fichas'  => $fichas
-    ]);
 });
 
 function posicionInicialFichas($idPartida, $jugadorIni, $jugadorEle ){
@@ -110,21 +120,22 @@ function posicionInicialFichas($idPartida, $jugadorIni, $jugadorEle ){
     $fichasJug1 = new fichas;
         $fichasJug1['jugador'] = $jugadorIni;
         $fichasJug1['idPartida'] = $idPartida;         
-        $fichasJug1['pos'] = 'A4';       
+        $fichasJug1['pos'] = '15';       
     $fichasJug1->save();
 
     $fichasJug2 = new fichas;
         $fichasJug2['jugador'] = $jugadorEle;
         $fichasJug2['idPartida'] = $idPartida;         
-        $fichasJug2['pos'] = 'H5';      
+        $fichasJug2['pos'] = '85';      
     $fichasJug2->save();
 
     $ficha1 = $fichasJug1['pos'];
     $ficha2 = $fichasJug2['pos'];
 
     $fichas = [];
-    $fichas[1]  = $ficha1;
+    $fichas[1]  = $ficha1;   
     $fichas[2]  = $ficha2;
+   
     
     return $fichas;
      
@@ -133,18 +144,18 @@ function posicionInicialFichas($idPartida, $jugadorIni, $jugadorEle ){
 /*
 * envia turno y movimiento del jugador 
 */     
-Route::POST('/turno', function(Request $request) {
+Route::POST('/movimiento', function(Request $request) {
 
-	$partida = $request->only('jugador', 'idPartida', 'nuevaPos', 'estado', 'idFicha');
+	$partida = $request->only('idFicha1', 'idFicha2','estado', 'idPartida', 'posNueva');
 	
-		if(($partida['jugador'] == 1) && ($partida['estado']%2 != 0 )){ // turno del jugador 1 !!! 
+		if(($partida['estado'] == 1) || ($partida['estado']%2 != 0 )){ // turno del jugador 1 !!! 
     		$ok = partidas::where('idPartida', $partida['idPartida'])->update(['estados' => $partida['estado']]);
-    		$ok1 = fichas::where('idFicha', $partida['idFicha'] )->update(['pos' => $partida['nuevaPos']]);
+    		$ok1 = fichas::where('idFicha', $partida['idFicha1'] )->update(['pos' => $partida['posNueva']]);
     		$estado = "movimiento ok";
         	
-		}if(($partida['jugador'] == 2) && ($partida['estado']%2 == 0)){ // turno del jugador 2 !!! 
+		}if($partida['estado']%2 == 0){ // turno del jugador 2 !!! 
     		$ok =  partidas::where('idPartida', $partida['idPartida'] )->update(['estados' => $partida['estado']]);
-    		$ok1 = fichas::where('idFicha', $partida['idFicha'] )->update(['pos' => $partida['nuevaPos']]);
+    		$ok1 = fichas::where('idFicha', $partida['idFicha2'] )->update(['pos' => $partida['posNueva']]);
     		$estado = "movimiento ok";
 		}else{
 		    $estado = "Es el turno del otro jugador";   
